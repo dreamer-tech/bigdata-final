03381b03e7b3a5000240612b01bbf4083a4d5387
blackjack <deaddreamer777@gmail.com>
2023-09-26 16:34:06 +0300

    init

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+
+rm -f ./data/*.zip
+rm -f ./data/*.json
+rm -f ./data/*.csv
+
+fileid="1Y00kXVeymQGFPSasl03dt7ygpeiYNxDc"
+filename="archive.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+unzip -o ./${filename} -d ./data
+
+rm cookie
+rm ${filename}
+rm -f ./output/*.csv
+rm -f ./output/*.json
+rm -f ./output/*.log
---
+++ sql/db.sql
@@ -0,0 +1,45 @@
+--\c project;
+
+-- Optional
+START TRANSACTION;
+
+create table artists(
+	artist_id text not null primary key,
+	followers float,
+	genres text[],
+    artist_name string,
+    popularity integer
+);
+
+create table tracks(
+	track_id integer not null primary key,
+	track_name integer,
+	popularity integer,
+	duration_ms integer,
+	explicit integer,
+	artists text[],
+	id_artists text[],
+	release_date date,
+	danceability float,
+	energy float,
+	track_key integer,
+    loudness float,
+    mode integer,
+    speechiness float,
+    acousticness float,
+    instrumentalness float,
+    liveness float,
+    valence float,
+    tempo float,
+    time_signature integer
+);
+
+SET datestyle TO iso, ymd;
+
+\COPY artists from 'data/artists.csv' delimiter ',' CSV header null as 'null';
+\COPY tracks from 'data/tracks.csv' delimiter ',' CSV header null as 'null';
+
+commit;
+
+select * from tracks limit 5;
+select * from artists limit 5;
