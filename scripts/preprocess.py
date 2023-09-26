80883e2eb5b4ac022f826133b3d77f89df6716b8
blackjack <deaddreamer777@gmail.com>
2023-09-26 17:54:32 +0300

    upd

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,9 +1,19 @@
 import pandas as pd
 
 
+def convert_date(d: str):
+    if d.count('-') == 0:
+        return d + '-01-01'
+    return d
+
+
 if __name__ == '__main__':
     artists_df = pd.read_csv('./data/artists.csv')
+    tracks_df = pd.read_csv('./data/tracks.csv')
 
     artists_df['followers'].fillna(0.0, inplace=True)
 
+    tracks_df['release_date'] = tracks_df['release_date'].apply(convert_date)
+
     artists_df.to_csv("./data/artists.csv", index=False)
+    tracks_df.to_csv("./data/tracks.csv", index=False)
