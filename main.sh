dcd02941434a92b7d2f2bad1f3354dd0aed2f12e
blackjack <deaddreamer777@gmail.com>
2023-09-27 15:10:49 +0300

    upd

--- main.sh
+++ main.sh
@@ -11,21 +11,21 @@ bash scripts/stage1.sh
 echo "Running Stage 2 of the pipeline - Hive + Spark SQL"
 bash scripts/stage2.sh
 
-#echo "Running Stage 3 of the pipeline - Spark ML"
-#bash scripts/stage3.sh
-#
+echo "Running Stage 3 of the pipeline - Spark ML"
+bash scripts/stage3.sh
+
 #echo "Running Stage 4 of the pipeline - Streamlit"
 #bash scripts/stage4.sh
-#
-## Post-processing
-#echo "Running post-processing!"
-#bash scripts/postprocess.sh
-#
-#
-## Check the quality of the codes
-#echo "The quality of scripts in 'scripts/' folder\n"
-#echo "::============================================::"
-#pylint scripts
-#
-#
-#echo "Done testing the pipeline!"
+
+# Post-processing
+echo "Running post-processing!"
+bash scripts/postprocess.sh
+
+
+# Check the quality of the codes
+echo "The quality of scripts in 'scripts/' folder\n"
+echo "::============================================::"
+pylint scripts
+
+
+echo "Done testing the pipeline!"
